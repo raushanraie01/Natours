0187d2b7ffe41ec08bb96104260cd09ffb748c60
raushanraie01 <raushanraie01@gamil.com>
2025-04-02 16:19:15 +0530

    initaialized and starts developing

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const morgan = require('morgan');
-const tourRoute = require('./routes/tourRoutes');
-const userRoute = require('./routes/userRoutes');
 
 const app = express();
 //Middleware
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Hello from middleware');
-  next();
-});
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
 
-  next();
+app.get('api/v1/tours', (req, res) => {
+  res.send();
 });
-app.use('/api/v1/tours', tourRoute);
-app.use('/api/v1/users', userRoute);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const app = require('./app');
 
 mongoose
   .connect(
-    'mongodb+srv://raushanraie01:KOpwW93WU0aWSJ6z@udemyclouster0.pgj0n.mongodb.net/',
+    'mongodb+srv://raushanraie01:JstsKLazCuwye7lF@cluster0.ykly0ja.mongodb.net/TravellersData',
   )
   .then(() => console.log('Database Connected!'))
   .catch((err) => {
     console.log('Error!');
   });
 
-dotenv.config({ path: './config.env' });
-
-const PORT = Number(process.env.PORT) || 3002;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server app listening on port:${PORT} `);
 });
