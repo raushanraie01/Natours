e65373d2bf90d9466bdcfb3966ee51e8b9607ebf
raushanraie01 <raushanraie01@gamil.com>
2025-04-02 19:15:30 +0530

    add tour model and routes for creating tours

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
-const TourSchema = new mongoose.Schema({});
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour name must reuired'],
+    unique: [true, 'Tour name must be unique'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have price'],
+  },
+});
 
-const Tour = mongoose.model('Tours', TourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const Tour = require('../models/tour.model.js');
 
 const router = express.Router();
 
 router.route('/').get((_, res) => {
   res.status(200).json({
     status: 'success',
+  });
+});
+router.route('/').post((req, res) => {
+  // const { name, price, rating } = req.body();
+  const tour = Tour.create({
+    name: 'Raushan',
+    price: 300,
+    rating: 5,
+  });
+  console.log(tour);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Done',
   });
 });
 
